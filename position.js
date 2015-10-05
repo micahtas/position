07dac617db3c866439418f24c08da1e4d4992034
Micah <micahtas@hawaii.edu>
2015-10-04 21:16:21 -1000

    delete intellij project files

--- position.js
+++ position.js
@@ -7,7 +7,7 @@ var testlist = [1, 3, 5, 6];
 function position(list, target)
 {
   var i = 0;
-  while((i < list.length) &&(list[i] <= target))
+  while((i < list.length) && (list[i] <= target))
   {
     if(target === list[i])
     {
